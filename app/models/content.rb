00f008b53db27e27428a60e44c18de09a2001be0
Otsuri <k_henshin_5tenn5@yahoo.co.jp>
2020-05-04 18:24:38 +0000

    showアクションのTagのview調整(多対多

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,7 +3,6 @@ class Content < ApplicationRecord
   
   has_many :contents_ttags
   has_many :ttags, through: :contents_ttags, dependent: :destroy
-  # accepts_nested_attributes_for :contents_ttags
   
   mount_uploader :image, ImageUploader
   validates :title, presence: true
--- app/models/ttag.rb
+++ app/models/ttag.rb
@@ -1,4 +1,6 @@
 class Ttag < ApplicationRecord
   has_many :contents_ttags
   has_many :contents, through: :contents_ttags
+  
+  validates :name, presence: true
 end
