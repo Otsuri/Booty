a8659e28b5a1cc7dcb28dd0431f7359f92af3788
Otsuri <k_henshin_5tenn5@yahoo.co.jp>
2020-05-09 14:22:03 +0000

    tagsのバリデーションを改良(いくつかの記号を許可

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many :contents_tags
   has_many :contents, through: :contents_tags
   
-  validates :name, presence: true, length: { maximum: 100 }, format: { with: /\A[a-zA-Z0-9ぁ-んァ-ン一-龥]+\z/ }
+  validates :name, presence: true, length: { maximum: 100 }, format: { with: /\A[a-zA-Z0-9ぁ-んァ-ン一-龥ー・_()]+\z/ }
 end
